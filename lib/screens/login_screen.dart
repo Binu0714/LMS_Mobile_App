5802a569307ae02bc780cb3795b673af79a4c9ad
Binu0714 <binuj714@gmail.com>
2026-03-31 21:44:16 +0530

    feat(ui): implement basic login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Welcome Back.!",style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 40),
+
+            TextField(decoration: InputDecoration(labelText: 'Email', border: OutlineInputBorder())),
+            const SizedBox(height: 20),
+
+            TextField(obscureText: true, decoration: InputDecoration(labelText: 'Password', border: OutlineInputBorder())),
+            const SizedBox(height: 30),
+
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(onPressed: () {},child: const Text('Login')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
