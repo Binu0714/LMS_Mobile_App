09e3a770f6da4c67f1981ef0a4ed8066b50d5d26
Binu0714 <binuj714@gmail.com>
2026-04-06 09:16:15 +0530

    refactor(model): create course model for consistency

---
+++ lib/models/course.dart
@@ -0,0 +1,16 @@
+class Course {
+  final String title;
+  final String instructor;
+  final String description;
+  final String image;
+  final List<String> lessons;
+
+  Course({
+    required this.title,
+    required this.instructor,
+    required this.description,
+    required this.image,
+    required this.lessons,
+  });
+  
+}
